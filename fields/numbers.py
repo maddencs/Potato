f348427cada1a5a54be23041357e1b862ba4eaa9
cory <maddencs@gmail.com>
2015-08-06 09:59:02 -0700

    started fields, just testing submodule for other project

---
+++ fields/numbers.py
@@ -0,0 +1,16 @@
+class Field(object):
+
+    def __init__(self, maximum=None, minimum=None,)
+        self.maximum = maximum
+        self.minimum = minimum
+
+
+class IntField(Field):
+    name = 'Integer Field'
+    sql_type = 'integer'
+    null = False
+
+    def sql(self, field_name):
+        return sql_type + ' CHECK ' + '(' + field_name + '<' + self.maximum
+
+
